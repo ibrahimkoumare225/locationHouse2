584dca06acda9de53f3cd70eb45f363aa02477e4
Koumare <ibrahimkoumare5@gmail.com>
2024-03-19 00:10:40 +0100

    Ajout d’un paramètre d’entrée au composant

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+    @Input() housingLocation!: HousingLocation;
+  
 }
